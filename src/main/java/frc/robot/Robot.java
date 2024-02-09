4c5833f3a3f0451147ac2aebc7899c8184682c9e
grumpyCapuchins <jenkinsaustin708@gmail.com>
2024-02-08 20:01:43 -0500

    Polished Autonomous Classes, Created and Deleted Comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.util.sendable.SendableRegistry;
 //import edu.wpi.first.wpilibj.PWM;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
@@ -29,41 +30,77 @@ public class Robot extends TimedRobot {
   private final CANSparkMax m_backLeftMotor = new CANSparkMax(2, MotorType.kBrushed);
   private final CANSparkBase m_frontRightMotor = new CANSparkMax(4, MotorType.kBrushed);
   private final CANSparkMax m_backRightMotor = new CANSparkMax(3, MotorType.kBrushed);
+
+  //Constructs and initializes a Timer Object
+  private final Timer m_Timer = new Timer();
   
   //constructs and initializes a differential drive object
-  private final DifferentialDrive m_robotDriveLeft = new DifferentialDrive(m_frontLeftMotor::set, m_frontRightMotor::set);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_frontLeftMotor::set, m_frontRightMotor::set);
   
   //constructs an xbox controller object
   private XboxController m_Controller;
 
+  /**
+   * Robot class that stores all information that needs to be transfered to the super class
+   */
   public Robot() {
     //adds child to sendable Registry (Whatever that means)
-    SendableRegistry.addChild(m_robotDriveLeft, m_frontLeftMotor);
-    SendableRegistry.addChild(m_robotDriveLeft, m_frontRightMotor);
+    SendableRegistry.addChild(m_robotDrive, m_frontLeftMotor);
+    SendableRegistry.addChild(m_robotDrive, m_frontRightMotor);
   }
 
+  /**
+   * Initialization class that allows for set up for later code
+   */
   @Override
   public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
+    // Followers (Replacment for motor controller groups)
     m_backLeftMotor.follow(m_frontLeftMotor);
     m_backRightMotor.follow(m_frontRightMotor);
+
+    // inverts Right side of robot
     m_frontRightMotor.setInverted(true);
+
+    // starts live view from robot webcam
     CameraServer.startAutomaticCapture();
   }
 
+  /**
+   * Autonomous initialization class that creates objects or resets certain variables
+   * for autonomous Periodic
+   */
+  @Override
+  public void autonomousInit() {
+    // resets timer for autonomous
+    m_Timer.restart();
+  }
+
+  /**
+   * Program that is ran when the robot is in "Autonomous Mode"
+   */
   @Override
   public void autonomousPeriodic() {
-    
+    // if statment to show how long robot should run for 2 seconds (timer loop)
+    if (m_Timer.get() < 2.0) {
+      // tankDrive command that allows robot to run at 0.5 speed on each side
+      m_robotDrive.tankDrive(0.5, 0.5);
+    } else {
+      // stops robot
+      m_robotDrive.stopMotor();
+    }
   }
   
+  /**
+   * Program that is ran when robot is in "Teleoperated Mode"
+   */
   @Override
   public void teleopPeriodic() {
+    //initializes Controller Object
+    m_Controller = new XboxController(0);
+
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_Controller = new XboxController(0);
-    m_robotDriveLeft.arcadeDrive(m_Controller.getLeftY()/1, m_Controller.getRightX()/1);
+    m_robotDrive.arcadeDrive(m_Controller.getLeftY()/1, m_Controller.getRightX()/1);
   }
 }
